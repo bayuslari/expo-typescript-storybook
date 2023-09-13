0c462fce9c877828708b9dda2681c483114a7b43
Bayu S. Riyadi <bayu93mx@gmail.com>
2023-09-13 20:47:45 +0700

    Upgrade Storybook. Closes #1 (#2)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,17 @@
 module.exports = {
   stories: ["../components/*.stories.?(ts|tsx|js|jsx)"],
+
   addons: [
     "@storybook/addon-ondevice-controls",
     "@storybook/addon-ondevice-actions",
   ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
